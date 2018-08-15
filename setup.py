bf13ea0bd436c7f17031c55dc4adbdd395459126
Toby Harradine <tobyharradine@gmail.com>
2018-08-15 19:14:28 +1000

    Refactor setup metadata

--- setup.py
+++ setup.py
@@ -1,20 +1,3 @@
 from setuptools import setup
-from redgettext import __version__
 
-with open("README.rst") as file:
-    readme = file.read()
-
-setup(
-    name="redgettext",
-    author="Tobotimus",
-    description="A slightly modified pygettext for Red-DiscordBot",
-    long_description=readme,
-    url="https://github.com/Tobotimus/redgettext",
-    version=__version__,
-    license="GPL-3.0",
-    install_requires=[],
-    py_modules=["redgettext"],
-    namespace_packages=[],
-    include_package_data=True,
-    entry_points={"console_scripts": ["redgettext=redgettext:main"]}
-)
+setup()
