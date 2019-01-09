b044dba092043d78f5a737d2ae759348393a2f41
Toby Harradine <tobyharradine@gmail.com>
2019-01-10 10:47:48 +1100

    Bump version to 2.2
    Signed-off-by: Toby Harradine <tobyharradine@gmail.com>

--- redgettext.py
+++ redgettext.py
@@ -29,7 +29,7 @@ import tokenize
 from collections import OrderedDict
 from typing import Any, Callable, Dict, List, NamedTuple, Optional, TextIO, Tuple
 
-__version__ = "2.1"
+__version__ = "2.2"
 
 DEFAULT_KEYWORDS = ["_"]
 
