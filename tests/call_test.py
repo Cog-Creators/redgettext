df6724524b32396ebff0b4d078a04adec8e47bb7
Jakub Kuczys <me@jacken.men>
2022-09-03 01:55:59 +0200

    Add a test for keyword info resolution

--- tests/call_test.py
+++ tests/call_test.py
@@ -1,4 +1,6 @@
+import ast
 from textwrap import dedent
+from typing import Tuple
 
 import polib
 import pytest
@@ -59,3 +61,53 @@ class TestGettextCalls:
         assert extractor.potfile_manager.current_potfile == get_test_potfile(
             polib.POEntry(msgid="bar", occurrences=[(FILENAME, 1)])
         )
+
+
+def parse_call(source: str, entry: polib.POEntry) -> Tuple[str, ast.Call, polib.POEntry]:
+    node = ast.parse(source).body[0].value
+    assert type(node) is ast.Call
+    return source, node, entry
+
+
+class TestKeywordSpecGettextCalls:
+    OPTIONS = Options(
+        keyword_specs=(
+            "_:2,2t",
+            "_:1,2,3t",
+            "_:1,2,3c,4t",
+            "_:1",
+        ),
+    )
+
+    @pytest.mark.parametrize(
+        "source,node,entry",
+        (
+            parse_call(
+                '_("singular")', polib.POEntry(msgid="singular", occurrences=[(FILENAME, 1)])
+            ),
+            parse_call(
+                '_("ignored", "singular")',
+                polib.POEntry(msgid="singular", occurrences=[(FILENAME, 1)]),
+            ),
+            parse_call(
+                '_("singular", "plural", "ignored")',
+                polib.POEntry(
+                    msgid="singular", msgid_plural="plural", occurrences=[(FILENAME, 1)]
+                ),
+            ),
+            parse_call(
+                '_("singular", "plural", "context", "ignored")',
+                polib.POEntry(
+                    msgid="singular",
+                    msgid_plural="plural",
+                    msgctxt="context",
+                    occurrences=[(FILENAME, 1)],
+                ),
+            ),
+        ),
+    )
+    def test_keyword_info_resolution(
+        self, source: str, node: ast.Call, entry: polib.POEntry
+    ) -> None:
+        extractor = get_extractor(source, self.OPTIONS)
+        assert extractor.potfile_manager.current_potfile == get_test_potfile(entry)
